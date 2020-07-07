94c2f8dc192f842d9e1f6305cc3dc19e301431b6
Cody Gilliam <codygilliam@macbook-pro.attlocal.net>
2020-07-07 13:13:16 -0400

    adding database

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,10 +1,23 @@
 const express = require("express");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const morgan = require("morgan");
 // const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3030;
+
+mongoose.connect(
+  "mongodb+srv://codyg7:Zfri11-02@cluster0.9d8iz.mongodb.net/codyg7?retryWrites=true&w=majority",
+  {
+    userNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+const connection = mongoose.connection;
+
+connection.once("open", function () {
+  console.log("MongoDB database connection established successfully");
+});
 
 // HTTP request logger
 app.use(morgan("tiny"));
