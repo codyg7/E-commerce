b9ca52302e38a96895fed46ecb99780cf1f54a1b
Cody Gilliam <codygilliam@macbook-pro.attlocal.net>
2020-07-10 15:45:41 -0400

    .gitignore file fixed

---
+++ client/build/precache-manifest.0de1317dd2042874b03c251cddfec949.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3aef8b445c9dfd3595c27d0c5473579",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80df37755b89c5790516",
+    "url": "/static/css/main.7786afb9.chunk.css"
+  },
+  {
+    "revision": "b55fa5f3edaf6f3a9efa",
+    "url": "/static/js/2.a79dade8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a79dade8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80df37755b89c5790516",
+    "url": "/static/js/main.40e4afd5.chunk.js"
+  },
+  {
+    "revision": "39d502c0600acb131b17",
+    "url": "/static/js/runtime-main.a3934485.js"
+  },
+  {
+    "revision": "2ca3469891bf002b0fce6eea821bab39",
+    "url": "/static/media/1.2ca34698.jpg"
+  },
+  {
+    "revision": "69331357f60a63bf6189cc919bd25e9b",
+    "url": "/static/media/2.69331357.jpg"
+  },
+  {
+    "revision": "4cad4d43aa4ccca5829516289f6f20bc",
+    "url": "/static/media/3.4cad4d43.jpg"
+  },
+  {
+    "revision": "c2602cf704823a1325731bb9770fda87",
+    "url": "/static/media/4.c2602cf7.jpg"
+  },
+  {
+    "revision": "1e898897357df9750cf367def2bae8ae",
+    "url": "/static/media/5.1e898897.jpg"
+  }
+]);
