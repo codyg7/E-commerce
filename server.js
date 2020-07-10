cb730414a4e1a14f8d4d27d1e3c4144c2fa0f6e5
Cody Gilliam <codygilliam@macbook-pro.attlocal.net>
2020-07-10 16:09:48 -0400

    fixing server

--- server.js
+++ server.js
@@ -28,10 +28,10 @@ app.use(morgan("tiny"));
 app.use("/api", routes);
 
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "build")));
+  app.use(express.static(path.join(__dirname, "./client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "./client/build", "index.html"));
   });
 }
 
