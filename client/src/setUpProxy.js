abbc92925706d293495ad656608bee4fbd441f44
Cody Gilliam <codygilliam@macbook-pro.attlocal.net>
2020-07-10 16:25:12 -0400

    fixing proxy

--- client/src/setUpProxy.js
+++ client/src/setUpProxy.js
@@ -1,5 +1,5 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  app.use(createProxyMiddleware("/api", { target: "http://localhost:3030" }));
+  app.use(createProxyMiddleware("/api", { target: process.env.PORT }));
 };
