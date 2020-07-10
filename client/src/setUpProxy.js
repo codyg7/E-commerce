211398f3e156a5e1cb1e5384ff548897cf7e9b6b
Cody Gilliam <codygilliam@macbook-pro.attlocal.net>
2020-07-10 16:40:59 -0400

    fixing proxy pt3

--- client/src/setUpProxy.js
+++ client/src/setUpProxy.js
@@ -1,5 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  app.use(createProxyMiddleware("/save", { target: "http://localhost:3030" }));
+  app.use(
+    createProxyMiddleware("/api/save", { target: "http://localhost:3030" })
+  );
 };
